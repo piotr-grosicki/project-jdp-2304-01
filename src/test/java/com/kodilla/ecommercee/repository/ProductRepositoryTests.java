6fac7d732327843cae7cfa966a2cdde71d768c95
Robert Lewandowski <rlewandowski232@gmail.com>
2023-04-17 17:38:23 +0200

    JDP230401-17 Added ProductRepositoryTests_v666

--- src/test/java/com/kodilla/ecommercee/repository/ProductRepositoryTests.java
+++ src/test/java/com/kodilla/ecommercee/repository/ProductRepositoryTests.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +20,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class ProductRepositoryTests {
 
     @Autowired

4e2dd7e6f6a53231f226597f2fd585f3fa281e9f
Jakub Hryckiewicz <jak-hry@wp.pl>
2023-04-24 01:32:09 +0200

    JDP230401-25 - UserController implementation v2

--- src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
